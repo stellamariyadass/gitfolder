fc7e4cb6d87dc43830b26865927afdf08d28e1fe
stellamariyadass <stellaabinaya2705@gmail.com>
2025-05-28 16:03:38 +0530

    3rd commit

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package com.demogit;
+
+public class Demo {
+public static void main(String[] args) {
+	System.out.println("pooja");
+	System.out.println("Loganathan");
+	System.out.println("abi");
+	System.out.println("reena");
+	System.out.println("stella");	
+}
+}
